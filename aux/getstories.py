a76eba58d8ac8e606670f1f1238c0652a8f2be23
Francisco <fmamp24@gmail.com>
2023-10-11 02:06:38 +0100

    refactored script for stop words

--- aux/getstories.py
+++ aux/getstories.py
@@ -1,8 +1,9 @@
 import os
 import json
+import spacy
 
 # Define the path to the stories directory
-stories_dir = os.path.join(os.getcwd(), 'data/collected', 'stories')
+stories_dir = os.path.join(os.getcwd(), '../data/collected', 'stories')
 
 # Define the path to the output file
 output_file = os.path.join(os.getcwd(), 'output.txt')
@@ -17,6 +18,19 @@ with open(output_file, 'w') as f:
                 # Load the JSON data
                 data = json.load(json_file)
                 # Write the title and content to the output file and ignore any kind of HTML tags
-                f.write(data['title'] + '\n')
-                f.write(data['content_raw'] + '\n')
-                f.write('\n')
+                nlp = spacy.load('en_core_web_lg') 
+                
+                stop_words = spacy.lang.en.stop_words.STOP_WORDS 
+
+                doc_title = nlp(data['title'])
+
+                filtered_tokens = [token for token in doc_title if not token.is_stop and not token.is_punct and not token.is_digit and not token.is_quote] 
+
+                f.write(' '.join([token.text for token in filtered_tokens]) + '\n')
+
+                doc_content = nlp(data['content_raw'])
+
+                filtered_tokens = [token for token in doc_content if not token.is_stop]
+
+                f.write(' '.join([token.text for token in filtered_tokens]) + '\n')
+ 
