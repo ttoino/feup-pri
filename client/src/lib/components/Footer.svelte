0b188304a109fec02cf57f3772c8be34e7bb9dff
Mariana <73724268+golangis@users.noreply.github.com>
2023-12-14 00:15:41 +0000

    changes to header

--- client/src/lib/components/Footer.svelte
+++ client/src/lib/components/Footer.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-    import { Icon, Home } from "svelte-hero-icons";
-</script>
-
 <footer class="flex flex-col items-center p-4">
     LoL Universe Indexing and Search isn't endorsed by Riot Games and doesn't
     reflect the views or opinions of Riot Games or anyone officially involved in
